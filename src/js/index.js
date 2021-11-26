720be01a3fb26114271faa4288cc558adcb9cbe8
pedromchd <pedromchd01@outlook.com>
2021-11-25 21:17:24 -0300

    header final e finalização do index e do login

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,23 @@ const password_label = document.login.querySelector('label[for="senha"] i');
 const password_input = document.login.senha;
 
 password_input.addEventListener('input', () => {
-  if (password_input.value.length) {
-    if (password_input.type === 'password') password_label.classList.value = 'fa fa-eye';
-    else password_label.classList.value = 'fa fa-eye-slash';
-  } else {
-    password_input.type = 'password';
-    password_label.classList.value = 'fa fa-lock';
-  }
+  const bool = password_input.value.length;
+  password_label.classList.toggle(
+    'fa-eye',
+    password_input.type === 'password' && bool
+  );
+  password_label.classList.toggle(
+    'fa-eye-slash',
+    password_input.type === 'text' && bool
+  );
+  password_label.classList.toggle('fa-lock', !bool);
+  if (!bool) password_input.type = 'password';
 });
 
 password_label.addEventListener('click', () => {
-  if (password_label.classList.contains('fa-eye')) {
-    password_input.type = 'text';
-    password_label.classList.value = 'fa fa-eye-slash';
-  } else
-  if (password_label.classList.contains('fa-eye-slash')) {
-    password_input.type = 'password';
-    password_label.classList.value = 'fa fa-eye';
-  }
+  const eye = password_label.classList.contains('fa-eye');
+  const slash = password_label.classList.contains('fa-eye-slash');
+  password_input.type = (eye) ? 'text' : 'password';
+  password_label.classList.toggle('fa-eye', slash);
+  password_label.classList.toggle('fa-eye-slash', eye);
 });
