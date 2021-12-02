377fc4beb264053973707d407f08934db08b9b51
pedromchd <pedromchd01@outlook.com>
2021-12-02 00:35:21 -0300

    commits diarios

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        '88': '22rem'
+        '88': '22rem',
+        '205': '51.25rem'
+      },
+      height: {
+        '110': '27.5rem'
+      },
+      maxWidth: {
+        '205': '51.25rem'
+      },
+      maxHeight: {
+        '110': '27.5rem'
       },
       transitionProperty: {
         'bg-color': 'background-color'
