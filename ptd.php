961bcab5a2b49354346c963b76b44d798c871608
Pedro Machado <pedromchd@outlook.com.br>
2022-11-15 10:50:50 -0300

    ptd setup

---
+++ ptd.php
@@ -0,0 +1,9 @@
+<?php
+
+require "./includes/config.php";
+
+include __DIR__ . "./includes/header.php";
+
+include __DIR__ . "./includes/visualiza.php";
+
+include __DIR__ . "./includes/footer.php";
