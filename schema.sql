96eda75c46af459d585593d82cf0a15bacddd055
Pedro Machado <pedromchd@outlook.com.br>
2022-11-20 11:29:56 -0300

    adicionando cursos do ptd

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 PRAGMA foreign_keys = OFF;
 
-DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS ptd;
 
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE usuario
+CREATE TABLE usuarios
 (
   "id" INTEGER NOT NULL,
   "username" VARCHAR(255) NOT NULL UNIQUE,
@@ -14,19 +14,19 @@ CREATE TABLE usuario
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE ptd
-(
-  "id" INTEGER NOT NULL,
-  "periodo" NUMERIC NOT NULL,
-  "status" INTEGER NOT NULL CHECK("status" BETWEEN 0 AND 5),
-  PRIMARY KEY ("id")
-);
+-- CREATE TABLE ptds
+-- (
+--   "id" INTEGER NOT NULL,
+--   "periodo" NUMERIC NOT NULL,
+--   "status" INTEGER NOT NULL CHECK("status" BETWEEN 0 AND 5),
+--   PRIMARY KEY ("id")
+-- );
 
-INSERT INTO usuario ("username", "fullname", "password") VALUES
+INSERT INTO usuarios ("username", "fullname", "password") VALUES
 ("pedro", "Pedro Machado", "pedro"),
 ("marcos", "Marcos Copello", "marcos"),
 ("antonella", "Antonella Cuello", "marcos");
 
-INSERT INTO ptd ("periodo", "status") VALUES
-("2022/1", "0"),
-("2022/2", "1");
+-- INSERT INTO ptd ("periodo", "status") VALUES
+-- ("2022/1", "0"),
+-- ("2022/2", "1");
