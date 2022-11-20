7cbabdb65865a64cfa994f8c52ab60255d420ca4
Pedro Machado <pedromchd@outlook.com.br>
2022-11-20 20:41:20 -0300

    tabs do ptd

--- includes/ptd/editar.php
+++ includes/ptd/editar.php
@@ -1,30 +1,74 @@
-<section
-  class="flex h-full w-full flex-col"
-  x-data="{
-    tab: 'cabecalho',
+<section class="flex h-full w-full flex-col" x-data="{
+    tab: 'Cabeçalho',
     toggle(id) { this.tab = id },
     isOpen(id) { return this.tab === id }
-  }"
->
+  }">
   <nav class="mb-4">
     <ul class="flex space-x-2 text-center font-semibold">
-      <li><button class="inline-block rounded-t-lg border-b-2 p-4" @click="toggle('cabecalho')">Cabeçalho</button></li>
-      <li><button class="inline-block rounded-t-lg border-b-2 p-4" @click="toggle('disciplinas')">Disciplinas</button></li>
       <li>
-        <button class="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:text-neutral-600 dark:hover:text-neutral-400">Dashboard</button>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Cabeçalho') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Cabeçalho')">
+          Cabeçalho
+        </button>
       </li>
       <li>
-        <button class="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:text-neutral-600 dark:hover:text-neutral-400">Settings</button>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Disciplinas') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Disciplinas')">
+          Disciplinas
+        </button>
       </li>
       <li>
-        <button class="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:text-neutral-600 dark:hover:text-neutral-400">Contacts</button>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Complementares') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Complementares')">
+          Complementares
+        </button>
+      </li>
+      <li>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Pesquisa') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Pesquisa')">
+          Pesquisa
+        </button>
+      </li>
+      <li>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Extensão') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Extensão')">
+          Extensão
+        </button>
+      </li>
+      <li>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Adminsitração') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Adminsitração')">
+          Adminsitração
+        </button>
+      </li>
+      <li>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Atividades') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Atividades')">
+          Atividades
+        </button>
+      </li>
+      <li>
+        <button class="inline-block rounded-t-lg border-b-2 p-4" :class="isOpen('Observações') ? '' : 'border-transparent hover:text-neutral-600 dark:hover:text-neutral-400'" @click="toggle('Observações')">
+          Observações
+        </button>
       </li>
     </ul>
   </nav>
-  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('cabecalho')">
-    <p class="text-neutral-600 dark:text-neutral-400">Cabeçalho</p>
-  </article>
-  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('disciplinas')">
-    <p class="text-neutral-600 dark:text-neutral-400">Disciplinas</p>
-  </article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Cabeçalho')">
+    <?php include __DIR__ . "./cabecalho.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Disciplinas')">
+    <?php include __DIR__ . "./disciplinas.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Complementares')">
+    <?php include __DIR__ . "./complementares.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Pesquisa')">
+    <?php include __DIR__ . "./pesquisa.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Extensão')">
+    <?php include __DIR__ . "./extensao.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Adminsitração')">
+    <?php include __DIR__ . "./adminsitracao.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Atividades')">
+    <?php include __DIR__ . "./atividades.php" ?>
+</article>
+  <article class="flex-grow rounded-lg bg-neutral-100 p-4 dark:bg-neutral-900" x-cloak x-show="isOpen('Observações')">
+    <?php include __DIR__ . "./observacoes.php" ?>
+</article>
 </section>
